8a6f8a0ac7fcb3f9ee6e0057e2ae26bfaf21b11b
Sabarish Gurajada Krishna <57188253+sabarish-gurajada@users.noreply.github.com>
2021-03-01 22:11:54 +0000

    Update run_import.sh

--- run_import.sh
+++ run_import.sh
@@ -1,1 +1,1 @@
-python import.py --instance_id=mohan-spanner-id --database_id=mohan_database --table_id=ImportTest --batchsize=2 --data_file=testdata.csv —-format_file=test.fmt
+python import.py --instance_id=mohan-spanner-id --database_id=mohan_database --table_id=ImportTest --batchsize=2 --data_file=testdata.csv --format_file=test.fmt
